7cddf3abba11088f18ce632d6dae3eb6633b610f
Pablo García Fernández <uo276824@uniovi.es>
2022-02-07 12:42:20 +0100

    SDI-2122-1008-2.1-Create Spring project.

---
+++ src/main/java/com/example/sdi21221008spring/Sdi21221008SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdi21221008spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi21221008SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi21221008SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdi21221008spring/Sdi21221008SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdi21221008spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi21221008SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
